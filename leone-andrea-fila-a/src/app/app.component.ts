b9bc4ca85571aeada765acace2935415b8ee1528
AndreaLeone06 <leone.andrea@iisgalvanimi.edu.it>
2023-12-14 13:23:04 +0000

    punto5

--- leone-andrea-fila-a/src/app/app.component.ts
+++ leone-andrea-fila-a/src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'leone-andrea-fila-a';
+  nome = "app"
 }
--- leone-andrea-fila-a/src/app/quarto/quarto.component.ts
+++ leone-andrea-fila-a/src/app/quarto/quarto.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./quarto.component.css']
 })
 export class QuartoComponent {
-
+  nome = "quarto"
 }
